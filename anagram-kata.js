a9a91fc16cecc3542f0d20a353b4a581cf49f9c5
Flavio Coscarella <flaviodc@live.com.ar>
2022-05-30 09:19:13 -0300

    Where my anagrams at? actualizado

--- anagram-kata.js
+++ anagram-kata.js
@@ -1,12 +1,15 @@
 // Kata finalizado el 20/05/2022
 
 function anagrams(word, words) {
-    let result = [];
-    // se separan las cadenas, se ordenan bajo un mismo criterio (sort) y luego se comparan
-    words.map(a => {
-        if (word.split("").sort().join("") === a.split("").sort().join("")) {
-            result.push(a);
-        }
-    })
-    return result;
+  let result = [];
+  // se separan las cadenas, se ordenan bajo un mismo criterio (sort) y luego se comparan
+  words.map((a) => {
+    if (
+      word.length === a.length &&
+      word.split("").sort().join("") === a.split("").sort().join("")
+    ) {
+      result.push(a);
+    }
+  });
+  return result;
 }
