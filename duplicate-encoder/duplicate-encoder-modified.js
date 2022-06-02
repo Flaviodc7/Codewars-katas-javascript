f2ea1229cd5eaae5d3f8b2ad0e4c3b304f70693d
Flavio Coscarella <flaviodc@live.com.ar>
2022-06-02 12:02:01 -0300

    Adding comments

--- duplicate-encoder/duplicate-encoder-modified.js
+++ duplicate-encoder/duplicate-encoder-modified.js
@@ -3,14 +3,18 @@
 function duplicateEncode(word) {
   let letters = [...new Set(word.toLowerCase())];
   let result = [];
+  // Mapping letter by letter
   letters.map((letter) => {
+    // Creating array with positions of the letter
     let arrayPosition = [];
+    // mapping the original string
     word
       .toLowerCase()
       .split("")
       .map((wordLetter, position) => {
         wordLetter === letter && arrayPosition.push(position);
       });
+      // if to put "(" if letter appears 1 time or ")" for 2 or more 
     if (arrayPosition.length === 1) {
       result[arrayPosition] = "(";
     } else {
